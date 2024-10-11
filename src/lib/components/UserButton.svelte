1748013d31005036ae930c345ee8148681128ae1
paul77uk <paul77uk@gmail.com>
2024-10-11 21:01:58 +0100

    set up sign in out button

--- src/lib/components/UserButton.svelte
+++ src/lib/components/UserButton.svelte
@@ -1,21 +1,40 @@
 <script>
 	import { CircleUserRound } from 'lucide-svelte'
 	import { isAuthenticated } from '../../routes/isAuthenticated.svelte'
+	import {
+		DropdownMenu,
+		DropdownMenuTrigger,
+		DropdownMenuContent,
+		DropdownMenuItem,
+	} from '$lib/components/ui/dropdown-menu'
+	import { LogOut, LogIn } from 'lucide-svelte'
+	import Button from './ui/button/button.svelte'
 
-  let isLoggedIn = isAuthenticated()
+	let isLoggedIn = isAuthenticated()
 </script>
 
-<ul class="text-white">
-	<li>
-		<a href="/api/auth/login">Sign in</a>
-	</li>
-	<li>
-		<a href="/api/auth/register">Sign up</a>
-	</li>
-	<li>
-		<a href="/api/auth/logout"> Sign out </a>
-	</li>
-	<li>{isLoggedIn.authState}</li>
-</ul>
-
-<CircleUserRound color={'white'} size={30} />
+{#if isLoggedIn.authState}
+	<DropdownMenu>
+		<DropdownMenuTrigger>
+			<CircleUserRound color={'white'} size={30} />
+		</DropdownMenuTrigger>
+		<DropdownMenuContent class="me-5 min-w-[4rem]">
+			<DropdownMenuItem>
+				<a
+					class="flex items-center justify-center gap-2 font-medium"
+					href="/api/auth/logout"
+				>
+					<LogOut size={16} />
+					Logout
+				</a>
+			</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>
+{:else}
+	<a href="/api/auth/login">
+		<Button class="flex gap-2 bg-[#DF0000] hover:bg-[#DF2222]">
+			<LogIn size={16} />
+			Login
+		</Button>
+	</a>
+{/if}
